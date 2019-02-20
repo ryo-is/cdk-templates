3bc0781a867f9d1bc7c2a6ff95099173e5979b90
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-02-20 13:02:29 +0900

    Initial commit

---
+++ bin/cdk-demo.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/cdk');
+import { CdkDemoStack } from '../lib/cdk-demo-stack';
+
+const app = new cdk.App();
+new CdkDemoStack(app, 'CdkDemoStack');
+app.run();
---
+++ lib/cdk-demo-stack.ts
@@ -0,0 +1,9 @@
+import cdk = require('@aws-cdk/cdk');
+
+export class CdkDemoStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
