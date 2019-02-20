8c225ceae80c1e2489b0de5bcde4cd4eee9f6edc
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-02-20 15:18:53 +0900

    feat:APIGatewayとLambdaをデプロイする

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/cdk');
-import { CdkDemoStack } from '../lib/cdk-demo-stack';
+import "source-map-support/register";
+import cdk = require("@aws-cdk/cdk");
+import { CdkDemoStack } from "../lib/cdk-demo-stack";
 
 const app = new cdk.App();
 new CdkDemoStack(app, 'CdkDemoStack');
--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,9 +1,31 @@
-import cdk = require('@aws-cdk/cdk');
+import cdk = require("@aws-cdk/cdk");
+// import s3 = require("@aws-cdk/aws-s3");
+import apigateway = require("@aws-cdk/aws-apigateway");
+import lambda = require("@aws-cdk/aws-lambda");
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // new s3.Bucket(this, "CDKDemoBucket", {
+    //   versioned: true
+    // });
+
+    const handler = new lambda.Function(this, "CdkLambdaDemo", {
+      runtime: lambda.Runtime.NodeJS810,
+      code: lambda.Code.directory("resources"),
+      handler: "index.demo",
+      environment: {
+        TZ: "Asia/Tokyo"
+      }
+    });
+
+    const api = new apigateway.RestApi(this, "CdkAPIDemo", {
+      restApiName: "Cdk Demo",
+      description: "AWS-CDKのデモ"
+    });
+
+    const getIntegration = new apigateway.LambdaIntegration(handler);
+    api.root.addMethod("GET", getIntegration);
   }
 }
