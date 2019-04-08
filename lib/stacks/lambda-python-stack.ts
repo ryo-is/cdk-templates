0381225d74684f994ffa881e7d4003c42a7c301f
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-08 11:50:35 +0900

    feat:Androidからテスト用API作成

--- lib/stacks/lambda-python-stack.ts
+++ lib/stacks/lambda-python-stack.ts
@@ -1,34 +1,40 @@
 import cdk = require("@aws-cdk/cdk");
+import apigateway = require("@aws-cdk/aws-apigateway");
 import lambda = require("@aws-cdk/aws-lambda");
-import dynamodb = require("@aws-cdk/aws-dynamodb");
-import iam = require("@aws-cdk/aws-iam");
+// import dynamodb = require("@aws-cdk/aws-dynamodb");
+// import iam = require("@aws-cdk/aws-iam");
 
 import { LambdaFunctionCreator } from "../services/lambda_function/creator";
-import { DynamoDBCreator } from "../services/dynamodb/creator";
-import { IAMCreator } from "../services/iam/creator";
+import { APIGatewayCreator } from "../services/apigateway/creator";
+// import { DynamoDBCreator } from "../services/dynamodb/creator";
+// import { IAMCreator } from "../services/iam/creator";
 
 export class PythonLambdaStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambdaFunction: lambda.Function = LambdaFunctionCreator.CreatePythonRuntimeLambdaFunction(this, "CdkPythonLambda", "index.handler");
+    const lambdaFunction: lambda.Function = LambdaFunctionCreator.CreatePythonRuntimeLambdaFunction(this, "CdkPythonLambda", "index.handler")
+    const apiGateway: apigateway.RestApi = APIGatewayCreator.CreateApiGateway(this, "CdkADeployedPI", "AWS-CDKでデプロイしたAPIGateway")
 
-    const tableParams: dynamodb.TableProps[] = [
-      {
-        tableName: "CDKPythonLambdaStackTable",
-        partitionKey: {
-          name: "ID",
-          type: dynamodb.AttributeType.String
-        },
-        sortKey: {
-          name: "record_time",
-          type: dynamodb.AttributeType.String
-        }
-      }
-    ];
-    const table: dynamodb.Table = DynamoDBCreator.CreateDynamoDB(this, tableParams[0]);
+    const integration = new apigateway.LambdaIntegration(lambdaFunction)
+    apiGateway.root.addResource("get").addMethod("GET", integration)
 
-    const statement: iam.PolicyStatement = IAMCreator.CreateDDBReadWriteRoleStatement(table.tableArn);
-    lambdaFunction.addToRolePolicy(statement);
+    // const tableParams: dynamodb.TableProps[] = [
+    //   {
+    //     tableName: "CDKPythonLambdaStackTable",
+    //     partitionKey: {
+    //       name: "ID",
+    //       type: dynamodb.AttributeType.String
+    //     },
+    //     sortKey: {
+    //       name: "record_time",
+    //       type: dynamodb.AttributeType.String
+    //     }
+    //   }
+    // ];
+    // const table: dynamodb.Table = DynamoDBCreator.CreateDynamoDB(this, tableParams[0]);
+
+    // const statement: iam.PolicyStatement = IAMCreator.CreateDDBReadWriteRoleStatement(table.tableArn);
+    // lambdaFunction.addToRolePolicy(statement);
   }
 }
