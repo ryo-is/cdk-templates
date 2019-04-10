164446df4350b53821ab919ef64d5957fb242f0b
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-10 13:53:08 +0900

    feat:APIKeyを有効化する

--- lib/stacks/lambda-python-stack.ts
+++ lib/stacks/lambda-python-stack.ts
@@ -14,15 +14,34 @@ export class PythonLambdaStack extends cdk.Stack {
     super(scope, id, props);
 
     const lambdaFunction: lambda.Function = LambdaFunctionCreator.CreatePythonRuntimeLambdaFunction(this, "CdkPythonLambda", "index.handler")
+    const lambdaFunctionImport: lambda.IFunction = lambda.Function.import(this, "PostLambda", {
+      functionArn: "arn:aws:lambda:ap-northeast-1:795622185554:function:CdkPythonLambda"
+    })
     const apiGateway: apigateway.RestApi = APIGatewayCreator.CreateApiGateway(this, "CdkADeployedPI", "AWS-CDKでデプロイしたAPIGateway")
 
+    const usagePlan: apigateway.CfnUsagePlan = new apigateway.CfnUsagePlan(this, "CDKUsagePlan", {
+      apiStages: [{
+        apiId: apiGateway.restApiId,
+        stage: (apiGateway.deploymentStage as apigateway.Stage).stageName
+      }]
+    })
+    const apiKey: apigateway.CfnApiKey = new apigateway.CfnApiKey(this, "CDKApiKey")
+    new apigateway.CfnUsagePlanKey(this, "CDKUsagePlanKey", {
+      keyId: apiKey.apiKeyId,
+      keyType: "API_KEY",
+      usagePlanId: usagePlan.usagePlanId
+    })
+
     const integration: apigateway.Integration = new apigateway.LambdaIntegration(lambdaFunction)
-    const getResourceApi: apigateway.Resource = apiGateway.root.addResource("get")
+    const postIntegration: apigateway.Integration = new apigateway.LambdaIntegration(lambdaFunctionImport)
+    const getResourceApi: apigateway.Resource = apiGateway.root.addResource("getReq")
     getResourceApi.addMethod("GET", integration)
     APIGatewayCreator.AddOptions(getResourceApi)
 
-    const postResourceApi: apigateway.Resource = apiGateway.root.addResource("post")
-    postResourceApi.addMethod("POST", integration)
+    const postResourceApi: apigateway.Resource = apiGateway.root.addResource("postReq")
+    postResourceApi.addMethod("POST", postIntegration, {
+      apiKeyRequired: true,
+    })
     APIGatewayCreator.AddOptions(postResourceApi)
 
     // const tableParams: dynamodb.TableProps[] = [
