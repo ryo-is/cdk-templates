effa1e689882ce0b7493bb42519d424c35e975af
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-05-20 12:59:24 +0900

    feat:add comment

--- lib/stacks/kansaits_demo_stack.ts
+++ lib/stacks/kansaits_demo_stack.ts
@@ -6,6 +6,7 @@ export class KansaitsDemoStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
+    // create Lambda Function
     const demoLambda: lambda.Function = new lambda.Function(this, "demoLambda", {
       functionName: "KansaitsDemoLambda",
       runtime: lambda.Runtime.Python37,
@@ -15,12 +16,15 @@ export class KansaitsDemoStack extends cdk.Stack {
       timeout: 29
     })
 
+    // create APIGateway
     const demoAPI: apigateway.RestApi = new apigateway.RestApi(this, "demoAPI", {
       restApiName: "KansaitsDemoAPIGateway"
     })
 
+    // init APIGateway integration
     const integretion: apigateway.Integration = new apigateway.LambdaIntegration(demoLambda)
 
+    // Add Resource and Method
     const getAPI: apigateway.Resource = demoAPI.root.addResource("get")
     getAPI.addMethod("GET", integretion, {
       apiKeyRequired: false
