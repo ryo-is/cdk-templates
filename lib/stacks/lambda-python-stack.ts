b9568a8203491dbb0ffd66c8c8916f8c12a226cf
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-05-28 15:13:31 +0900

    update aws-cdk0.32.0

--- lib/stacks/lambda-python-stack.ts
+++ lib/stacks/lambda-python-stack.ts
@@ -1,13 +1,13 @@
 import cdk = require("@aws-cdk/cdk")
 import apigateway = require("@aws-cdk/aws-apigateway")
 import lambda = require("@aws-cdk/aws-lambda")
-// import dynamodb = require("@aws-cdk/aws-dynamodb")
-// import iam = require("@aws-cdk/aws-iam")
+import dynamodb = require("@aws-cdk/aws-dynamodb")
+import iam = require("@aws-cdk/aws-iam")
 
 import { LambdaFunctionCreator } from "../services/lambda_function/creator"
 import { APIGatewayCreator } from "../services/apigateway/creator"
-// import { DynamoDBCreator } from "../services/dynamodb/creator"
-// import { IAMCreator } from "../services/iam/creator"
+import { DynamoDBCreator } from "../services/dynamodb/creator"
+import { IAMCreator } from "../services/iam/creator"
 
 export class PythonLambdaStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -52,22 +52,22 @@ export class PythonLambdaStack extends cdk.Stack {
     })
     APIGatewayCreator.AddOptions(postResourceApi)
 
-    // const tableParams: dynamodb.TableProps[] = [
-    //   {
-    //     tableName: "CDKPythonLambdaStackTable",
-    //     partitionKey: {
-    //       name: "ID",
-    //       type: dynamodb.AttributeType.String
-    //     },
-    //     sortKey: {
-    //       name: "record_time",
-    //       type: dynamodb.AttributeType.String
-    //     }
-    //   }
-    // ]
-    // const table: dynamodb.Table = DynamoDBCreator.CreateDynamoDB(this, tableParams[0])
+    const tableParams: dynamodb.TableProps[] = [
+      {
+        tableName: "CDKPythonLambdaStackTable",
+        partitionKey: {
+          name: "ID",
+          type: dynamodb.AttributeType.String
+        },
+        sortKey: {
+          name: "record_time",
+          type: dynamodb.AttributeType.String
+        }
+      }
+    ]
+    const table: dynamodb.Table = DynamoDBCreator.CreateDynamoDB(this, tableParams[0])
 
-    // const statement: iam.PolicyStatement = IAMCreator.CreateDDBReadWriteRoleStatement(table.tableArn)
-    // lambdaFunction.addToRolePolicy(statement)
+    const statement: iam.PolicyStatement = IAMCreator.CreateDDBReadWriteRoleStatement(table.tableArn)
+    lambdaFunction.addToRolePolicy(statement)
   }
 }
