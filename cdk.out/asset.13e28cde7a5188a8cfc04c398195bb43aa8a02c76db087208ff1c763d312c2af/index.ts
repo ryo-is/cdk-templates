a01debab81452fafc001cdf1c77778a445bc2d2f
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-06-18 11:56:33 +0900

    feat:画像を受け取ってレスポンスを返す

---
+++ cdk.out/asset.13e28cde7a5188a8cfc04c398195bb43aa8a02c76db087208ff1c763d312c2af/index.ts
@@ -0,0 +1,31 @@
+import {
+  Handler,
+  APIGatewayEvent,
+  Context,
+  Callback
+ } from "aws-lambda"
+
+export const handler: Handler = async (
+  event: APIGatewayEvent,
+  _context: Context,
+  callback: Callback
+) => {
+  console.log(JSON.stringify(event))
+  // return {
+  //   statusCode: 200,
+  //   body: {
+  //     message: "Success!!!",
+  //     input: JSON.stringify(event)
+  //   },
+  //   handlers: {
+  //     "Access-Control-Allow-Origin": "*"
+  //   }
+  // }
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(event.body),
+    handlers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  })
+}
---
+++ cdk.out/asset.8701f34dbe078f2603fe06d609b86351a297cba897d1c775eff2a03a23084247/index.ts
@@ -0,0 +1,17 @@
+import { Handler, APIGatewayEvent } from "aws-lambda"
+
+export const handler: Handler = async (
+  event: APIGatewayEvent
+) => {
+  console.log(JSON.stringify(event))
+  return {
+    statusCode: 200,
+    body: {
+      message: "Success!!!",
+      input: JSON.stringify(event)
+    },
+    handlers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  };
+}
