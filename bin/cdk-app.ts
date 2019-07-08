0483a87bba2a4dc8ce0a59fbf941117aa91c2e78
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-07-08 09:46:31 +0900

    feat:Stackを指定してdeployできるか確認

--- bin/cdk-app.ts
+++ bin/cdk-app.ts
@@ -1,20 +1,10 @@
 #!/usr/bin/env node
 import "source-map-support/register"
 import cdk = require("@aws-cdk/core")
-// import { CdkStack } from "../lib/stacks/cdk-stack"
-// import { KinesisAnalyticsStack } from "../lib/stacks/kinesis_analytics_stack"
-// import { CdkPipelineStack } from "../lib/stacks/cdk-pipeline-stack"
-// import { PythonLambdaStack } from "../lib/stacks/lambda-python-stack"
-// import { KansaitsDemoStack } from "../lib/stacks/kansaits_demo_stack"
-// import { PutJsonToDynamo } from "../lib/stacks/put_json_to_dynamo"
+import { CdkStack } from "../lib/stacks/cdk-stack"
 import { PutImageLambda } from "../lib/stacks/put_image_lambda"
 
 const app = new cdk.App()
-// new CdkStack(app, "CdkStack")
-// new KinesisAnalyticsStack(app, "KinesisAnalyticsStack")
-// new CdkPipelineStack(app, "CdkPipelineStack")
-// new PythonLambdaStack(app, "PythonLambdaStack")
-// new KansaitsDemoStack(app, "KansaitsDemoStack")
-// new PutJsonToDynamo(app, "PutJsonToDynamo")
+new CdkStack(app, "CdkStack")
 new PutImageLambda(app, "PutImageLambda")
 app.synth()
