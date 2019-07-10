a8dd10efae20fd9d199f24a0aebc036cb91a41c8
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-07-10 11:19:48 +0900

    lint周りの調査

--- lambdaSoruces/demo_function/index.ts
+++ lambdaSoruces/demo_function/index.ts
@@ -9,7 +9,7 @@ export const handler: Handler = async (
   event: APIGatewayEvent,
   _context: Context,
   callback: Callback
-) => {
+): Promise<void> => {
   console.log(JSON.stringify(event))
   callback(null, {
     statusCode: 200,
--- lib/services/lambda_function/creator.ts
+++ lib/services/lambda_function/creator.ts
@@ -2,24 +2,18 @@ import cdk = require("@aws-cdk/core")
 import { Function, Runtime, Code } from "@aws-cdk/aws-lambda"
 
 export class LambdaFunctionCreator {
-  /**
-   * Create Lambda Function
-   * @param {cdk.Construct} self
-   * @param {String} functionName
-   * @param {String} handler
-   * @param {String} codeDirectory
-   */
-  static createFunction(
+  // Create Lambda Function
+  public static createFunction(
     self: cdk.Construct,
-    functionName: string,
+    name: string,
     codeDirectory: string,
-    handler: string
+    handlerName: string
   ) {
-    return new Function(self, functionName, {
-      functionName: functionName,
+    return new Function(self, name, {
+      functionName: name,
       runtime: Runtime.NODEJS_10_X,
       code: Code.asset(codeDirectory),
-      handler: handler,
+      handler: handlerName,
       memorySize: 256,
       timeout: cdk.Duration.seconds(300),
       environment: {
