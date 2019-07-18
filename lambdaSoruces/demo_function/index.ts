e2dfa8d3694d5973114aeabeef5cb04a886e5f62
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-07-18 11:13:08 +0900

    demo用lambdaのソースコードを修正

--- lambdaSoruces/demo_function/index.ts
+++ lambdaSoruces/demo_function/index.ts
@@ -1,5 +1,7 @@
 import { Handler, APIGatewayEvent, Context, Callback } from "aws-lambda"
-import { LambdaReturnBody } from "./types"
+import { LambdaReturnBody, MessageType } from "./types"
+
+import { MessageCreator } from "./lib/message_cretor"
 
 export const handler: Handler = async (
   event: APIGatewayEvent,
@@ -7,10 +9,10 @@ export const handler: Handler = async (
   _callback: Callback
 ): Promise<LambdaReturnBody> => {
   console.log(JSON.stringify(event))
-  const msgBody: string = "SUCCEEDED!!!"
+  const message: MessageType = MessageCreator.create()
   return {
     statusCode: 200,
     headers: { "Access-Control-Allow-Origin" : "*" },
-    body: JSON.stringify({ message: msgBody })
+    body: JSON.stringify(message)
   }
 }
