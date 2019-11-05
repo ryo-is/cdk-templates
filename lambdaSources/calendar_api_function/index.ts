6db1fb0101625e634e494d6d572e373437d2c5f3
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-11-05 16:01:13 +0900

    feat:取得したイベント情報をテーブルにPUTする

--- lambdaSources/calendar_api_function/index.ts
+++ lambdaSources/calendar_api_function/index.ts
@@ -1,8 +1,11 @@
+import { DynamoDB } from "aws-sdk/clients/all"
 import { Handler, APIGatewayEvent } from "aws-lambda"
 import { calendar_v3 } from "googleapis"
 import keys from "./lib/service_user_keys"
 import { JWT } from "google-auth-library"
 import * as dayjs from "dayjs"
+
+const DDB = new DynamoDB.DocumentClient()
 
 export const handler: Handler = async (
   event: APIGatewayEvent
@@ -24,11 +27,37 @@ export const handler: Handler = async (
     const carendar = new calendar_v3.Calendar({})
     const events = await carendar.events.list({
       auth: jwtClient,
-      calendarId: "ryosuke.izumi62@gmail.com",
+      calendarId: "kyo_visitor@kyoso.co.jp",
       timeMin: startTime,
       timeMax: endTime
     })
     console.log(events.data.items)
+
+    if (events.data.items === undefined) return
+
+    const promises: any[] = []
+    events.data.items.forEach((item: calendar_v3.Schema$Event) => {
+      const param: DynamoDB.DocumentClient.PutItemInput = {
+        TableName: "VisitorManagement",
+        Item: {
+          id: item.id,
+          prace: "kyoto",
+          location: item.location,
+          start_time: dayjs(
+            (item.start as calendar_v3.Schema$EventDateTime).dateTime
+          ).format("YYYY/MM/DD HH:mm"),
+          end_time: dayjs(
+            (item.end as calendar_v3.Schema$EventDateTime).dateTime
+          ).format("YYYY/MM/DD HH:mm"),
+          event_summary: item.summary
+        }
+      }
+      console.log(param)
+      promises.push(DDB.put(param).promise())
+    })
+
+    // eslint-disable-next-line no-undef
+    await Promise.all(promises)
   } catch (err) {
     console.error(err)
   }
