73973c07b074c16c8b6a08c7bdfc26ab81a26e6b
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-11-11 14:06:35 +0900

    feat:APIGateway+Lambdaのパターンを簡単にデプロイできるようにclassを用意

--- bin/cdk-app.ts
+++ bin/cdk-app.ts
@@ -3,8 +3,10 @@ import "source-map-support/register"
 import cdk = require("@aws-cdk/core")
 import { VisitorManagementAppsync } from "../lib/stacks/visitor_management_appsync"
 import { CalendarAPIStack } from "../lib/stacks/calendar_api"
+import { RestAPIs } from "../lib/stacks/rest_apis"
 
 const app: cdk.App = new cdk.App()
 new VisitorManagementAppsync(app, "VisitorManagementAppsync")
 new CalendarAPIStack(app, "CalendarAPIStack")
+new RestAPIs(app, "RestAPIs")
 app.synth()
