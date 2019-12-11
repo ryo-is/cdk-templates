b88bc0ef4fa47b4437adf9b92eab8445eb5b058b
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-12-11 17:43:04 +0900

    feat:LambdaLayerのバージョンが消えないように対応

--- lib/stacks/lambda_layer.ts
+++ lib/stacks/lambda_layer.ts
@@ -6,9 +6,13 @@ export class LambdaLayerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    new LayerVersion(this, "NodeLambdaLayer", {
+    const lambdaLayer = new LayerVersion(this, "NodeLambdaLayer", {
       code: AssetCode.fromAsset("lambdaSources/demo_function"),
       compatibleRuntimes: [Runtime.NODEJS_12_X]
     })
+    const lambdaLayerResource = lambdaLayer.node.findChild(
+      "Resource"
+    ) as cdk.CfnResource
+    lambdaLayerResource.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN)
   }
 }
