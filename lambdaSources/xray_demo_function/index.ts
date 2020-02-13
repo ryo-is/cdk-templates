0784f7d43f57de4ad5b3dd9d8ca81f682b7e9ace
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2020-02-13 14:22:25 +0900

    feat:Queryの件数を増やした処理を追加

--- lambdaSources/xray_demo_function/index.ts
+++ lambdaSources/xray_demo_function/index.ts
@@ -22,6 +22,20 @@ export const handler: Handler = async (): Promise<void> => {
     }
     const data = await DDB.query(queryParam).promise()
     console.log(data)
+
+    const queryParam_2: DocumentClient.QueryInput = {
+      TableName: "iot-kyoto-data",
+      KeyConditionExpression: "#ID = :id",
+      ExpressionAttributeNames: {
+        "#ID": "ID"
+      },
+      ExpressionAttributeValues: {
+        ":id": "id001"
+      },
+      Limit: 100
+    }
+    const data_2 = await DDB.query(queryParam_2).promise()
+    console.log(data_2)
   } catch (err) {
     console.error(err)
   }
